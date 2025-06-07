68523d084a81d3eb9f2137a10b2e76b19a8e44da
Usernameorsmth <simidarawilliams@gmial.com>
2025-06-07 19:26:34 +0100

    DREAMZ

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-import random
-import time
+def hotel_cost(nights):
+  return 140*nights
 
+#Define a function called plane_ride_cost that takes a string, city, as input.
+def plane_ride_cost(city):
+  if "Charlotte" == city:
+    return 183
+  elif "Tampa" == city:
+    return 220
+  elif "Pittsburgh" == city:
+    return 222
+  elif "Los Angeles" == city:
+    return 475
 
-def getRandomDate(startDate, endDate):
-    print("Printing random date between", startDate, "and", endDate)
-    randGenerator = random.random()
-    dateFormat = "%m/%d/%Y"
-    starttime = time.mktime(time.strptime(startDate, dateFormat))
-    endtime = time.mktime(time.strptime(endDate, dateFormat))
-    randomTime = starttime + randGenerator * (endtime - starttime)
-    randomDate = time.strftime(dateFormat, time.localtime(randomTime))
-    return randomDate
+#Define a function called rental_car_cost with an argument called days
+def rental_car_cost(days):
+  if days>=7 :
+    return 40*days - 50
+  elif days>=3 :
+    return 40*days - 20
+  else:
+    
+    return 40*days
+  #Define a function called trip cost with argument day, money and city
+def trip_cost(city, days, spending_money):
+  return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
 
-print("Random Date = ", getRandomDate("1/1/2020", "12/12/2024"))
+print("Cost of car rental: ",rental_car_cost(6))
+
+print("Cost of plane ride: ",plane_ride_cost("Los Angeles"))
+
+print("Cost of hotel room: ", hotel_cost(7))
+
+print("Total cost of the trip:",trip_cost("Los Angeles",7,500))
+
+print(trip_cost("Tampa",6,500))
